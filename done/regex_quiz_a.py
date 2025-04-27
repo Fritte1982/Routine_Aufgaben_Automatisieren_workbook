16d1e9e82091812b324462017878c4b36f6d4506
Fritte1982 <richard.bieligk@gmail.com>
2025-04-27 15:27:35 +0200

    regex_strip_fnc complete

--- done/regex_quiz_a.py
+++ done/regex_quiz_a.py
@@ -16,3 +16,18 @@ else:
 # Todo Schreiben Sie eine Funktion, die einen String annimmt und das Gleiche macht wie die Stringmethode strip().
 #  Wird außer dem String kein anderes Argument übergeben, sollen alle Weißraumzeichen vom Anfang und Ende des Strings entfernt
 #  werden. Anderenfalls wird das im zweiten Argument übergebene Zeichen vom Anfang und Ende des Strings entfernt
+testtxt = " strip "
+original_regex = r"^(\s*)\b(.+)\b(\s*)$"
+def regex_strip(text:str, char:str="")->str:
+    start_regex = r"^(\s*)\b"
+    middle_regex = r"(.+)"
+    end_regex = r"\b(\s*)$"
+    gesamt_regex = start_regex + middle_regex + end_regex
+    subst =r"\2"
+    if char:
+        subst =f"{char}{subst}{char}"
+        result = re.sub(gesamt_regex, subst, text)
+    else:
+        result = re.sub(gesamt_regex, subst, text)
+    return result
+print(regex_strip(testtxt,"+"))
