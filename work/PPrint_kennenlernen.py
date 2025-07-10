2a9db4a72a20d46f458c55ce98d7919f00aa77fa
Fritte1982 <richard.bieligk@gmail.com>
2025-07-10 08:43:02 +0200

    beginning pformat-Tutorial

--- work/PPrint_kennenlernen.py
+++ work/PPrint_kennenlernen.py
@@ -1,5 +1,11 @@
 import pprint
+from settings import paths_attributes as paths
+
+CAT_FILE_PATH = paths.OUTPUT_FOLDER / "myCats.py"
 
 cats = [{"name": "Zophie", "desc": "chubby"},
         {"name": "Pooka", "desc": "fluffy"}]
 pprint.pprint(pprint.pformat(cats))
+
+with open(CAT_FILE_PATH, "w") as f:
+        f.write("cats = " + pprint.pformat(cats) + "\n")
